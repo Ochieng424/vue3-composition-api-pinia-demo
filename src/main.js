82f8bbf71a4142894af750e4680e34dd1203dbc9
derrick.ochieng <derrick.ochieng@libryo.com>
2022-06-04 16:23:25 +0300

    install and setup pinia

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* pinia */
+import { createPinia } from "pinia";
+
+let app = createApp(App);
+
+app.use(createPinia());
+app.mount('#app')
